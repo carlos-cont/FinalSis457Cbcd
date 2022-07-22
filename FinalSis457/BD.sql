0517c5541a3f1fb4509169584abb574413e2df3c
carlos-cont <contrerasdurancarlosbeimar@gmail.com>
2022-07-22 17:46:21 -0400

    instalacion de dependencias y Scaffold de base series

---
+++ FinalSis457/BD.sql
@@ -0,0 +1,34 @@
+﻿CREATE DATABASE FinalSis457Cbcd;
+USE [master]
+GO
+CREATE LOGIN [usrfinal ] WITH PASSWORD=N'12345678', 
+   DEFAULT_DATABASE=[FinalSis457Cbcd], 
+   CHECK_EXPIRATION=OFF, 
+   CHECK_POLICY=ON
+GO
+USE [FinalSis457Cbcd]
+GO
+CREATE USER [usrfinal ] FOR LOGIN [usrfinal ]
+GO
+USE [FinalSis457Cbcd]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [usrfinal ]
+GO
+DROP TABLE IF EXISTS Serie;
+
+CREATE TABLE Serie (
+  id INT IDENTITY(1, 1) PRIMARY KEY,
+  titulo VARCHAR(250) NOT NULL,
+  sinopsis VARCHAR(5000) NOT NULL,
+  director VARCHAR(100) NOT NULL,
+  duracion INT NOT NULL,
+  fechaEstreno DATE NOT NULL,
+  registroActivo BIT
+);
+CREATE TABLE Usuario (
+    [id]              INT           IDENTITY (1, 1) NOT NULL,
+    [usuario]         VARCHAR (12)  NOT NULL,
+    [clave]           VARCHAR (250) NOT NULL,
+    [rol] VARCHAR (20) NOT NULL,
+    [registroActivo]  BIT           NULL
+);
--- FinalSis457Cbcd/Program.cs
+++ FinalSis457Cbcd/Program.cs
@@ -1,7 +1,13 @@
+using FinalSis457Cbcd.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<FinalSis457CbcdContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
